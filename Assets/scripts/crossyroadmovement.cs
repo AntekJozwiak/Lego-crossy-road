86318b65d86b81c3002158dea7a9e195da65a63c
AntekJozwiak <90831173+AntekJozwiak@users.noreply.github.com>
2022-10-19 00:21:53 +0100

    more spooky lol

--- Assets/scripts/crossyroadmovement.cs
+++ Assets/scripts/crossyroadmovement.cs
@@ -12,6 +12,7 @@ public class crossyroadmovement : MonoBehaviour
     {
         //transform.position = new Vector3(transform.position.x -.5f, transform.position.y, transform.position.z);
     }
+    public LayerMask acid;
     public bool moving;
     Vector3 moveto;
     Quaternion rotateto;
@@ -25,26 +26,26 @@ public class crossyroadmovement : MonoBehaviour
         }
         if (!moving)
         {
-            if (Input.GetKeyDown("w"))
+            if (Input.GetKeyDown("w") || Input.GetKeyDown("up"))
             {
                 moveto = transform.position + new Vector3(0, 0, 1);
                 rotateto = Quaternion.Euler(0, 0, 0);
                 moving = true;
                 terraingen.GetComponent<TerrainGenerator>().SpawnTerrain(false);
             }
-            else if (Input.GetKeyDown("s"))
+            else if (Input.GetKeyDown("s") || Input.GetKeyDown("down"))
             {
                 moveto = transform.position - new Vector3(0, 0, 1);
                 rotateto = Quaternion.Euler(0, 180, 0);
                 moving = true;
             }
-            else if (Input.GetKeyDown("d"))
+            else if (Input.GetKeyDown("d") || Input.GetKeyDown("right"))
             {
                 moveto = transform.position + new Vector3(1, 0, 0);
                 rotateto = Quaternion.Euler(0, 90, 0);
                 moving = true;
             }
-            else if (Input.GetKeyDown("a"))
+            else if (Input.GetKeyDown("a") || Input.GetKeyDown("left"))
             {
                 moveto = transform.position - new Vector3(1,0,0);
                 rotateto = Quaternion.Euler(0, -90, 0);
@@ -63,6 +64,13 @@ public class crossyroadmovement : MonoBehaviour
             }
             
         }
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, Vector3.down, out hitInfo, Mathf.Infinity, acid))
+        {
+            Debug.DrawRay(transform.position, -transform.up);
+        }
+
+
     }
 
 }
